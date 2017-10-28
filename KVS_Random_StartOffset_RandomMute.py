9f75398325ae1ec30689a7434d83d9ca57a36c6a
Kyle Vande Slunt <kylevs@gmail.com>
2017-10-27 23:48:27 -0400

    added comments for Aaron in RandomOffset_RandomMute.py

--- KVS_Random_StartOffset_RandomMute.py
+++ KVS_Random_StartOffset_RandomMute.py
@@ -1,5 +1,26 @@
 from random import uniform
 
+
+""" Aaron - this is a simple script that will choose a random offset of the selected items. The media item stays in place but the position
+of the waveform changes. Throw any audio file into a track and run the script and you'll see the media item stay in it's location on the 
+timeline - but the waveform will jump around. the random_startoffset function works but we need to take into account the length of the SOURCE
+file - not the length of the item which i have here. A media item container can be longer than the actual source file itself which can cause 
+some complications
+
+The second function - random_mute - should be very simple and I can't get it to work. What I'd like it to do is - count how many items there
+and then mute a percentage of the total items every time.
+
+This script will yield a ton of different variations. Let's say I stack 8 audio files of different forest ambiences on top of each other - 
+each one being 5 minutes long. When you play them back - they all start at the beginning of the file. The random offset will provide variation
+by starting the file at different locations. So once you run random_startoffset, file 1 will start at 1:20, file 2 will start at 4:18, etc.
+
+Then the idea is the random_mute function will mute a couple of these files to thin out the mix and provide different variations. So when i run
+the script Files 2 and 4 will be muted, and then when i run it again Files 1 and 7 will be muted. 
+
+Does that make sense? Anyways - would love for us to work on this to ease back into cranking out some sweet scripts!
+
+
+"""
 def log(string):
     RPR_ShowConsoleMsg('{}{}'.format(string,'\n'))
 
