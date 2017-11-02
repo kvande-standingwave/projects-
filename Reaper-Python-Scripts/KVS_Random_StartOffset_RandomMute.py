a968099f94d4b2389a2e8e4fa30667784bb497d7
Kyle Vande Slunt <kylevs@gmail.com>
2017-11-01 22:59:10 -0400

    made random mute function work

--- Reaper-Python-Scripts/KVS_Random_StartOffset_RandomMute.py
+++ Reaper-Python-Scripts/KVS_Random_StartOffset_RandomMute.py
@@ -1,26 +1,7 @@
 from random import uniform
+from random import getrandbits
 
 
-""" Aaron - this is a simple script that will choose a random offset of the selected items. The media item stays in place but the position
-of the waveform changes. Throw any audio file into a track and run the script and you'll see the media item stay in it's location on the 
-timeline - but the waveform will jump around. the random_startoffset function works but we need to take into account the length of the SOURCE
-file - not the length of the item which i have here. A media item container can be longer than the actual source file itself which can cause 
-some complications
-
-The second function - random_mute - should be very simple and I can't get it to work. What I'd like it to do is - count how many items there
-and then mute a percentage of the total items every time.
-
-This script will yield a ton of different variations. Let's say I stack 8 audio files of different forest ambiences on top of each other - 
-each one being 5 minutes long. When you play them back - they all start at the beginning of the file. The random offset will provide variation
-by starting the file at different locations. So once you run random_startoffset, file 1 will start at 1:20, file 2 will start at 4:18, etc.
-
-Then the idea is the random_mute function will mute a couple of these files to thin out the mix and provide different variations. So when i run
-the script Files 2 and 4 will be muted, and then when i run it again Files 1 and 7 will be muted. 
-
-Does that make sense? Anyways - would love for us to work on this to ease back into cranking out some sweet scripts!
-
-
-"""
 def log(string):
     RPR_ShowConsoleMsg('{}{}'.format(string,'\n'))
 
@@ -34,8 +15,8 @@ def random_startoffset(items):
 
 def random_mute(items):
 	selected_item = RPR_GetSelectedMediaItem(0, items)
-	active_take = RPR_GetActiveTake(selected_item)
-	RPR_SetMediaItemTakeInfo_Value (active_take, 'B_MUTE', 1.0)
+	#active_take = RPR_GetActiveTake(selected_item)
+	RPR_SetMediaItemInfo_Value (selected_item, 'B_MUTE', bool(getrandbits(1)))
 	RPR_UpdateArrange()	
 
 
